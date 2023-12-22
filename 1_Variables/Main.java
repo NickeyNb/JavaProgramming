1fcb5fe5e2c2f4d3c0df4cbd7a9cd034f45d050e
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-22 15:33:02 +0530

    Initial commit

---
+++ 1_Variables/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Variables in Java");
+        
+        int age = 21;
+        double height = 5.5;
+        String name = "Bhupendra Jogi";
+
+        System.out.println("Name is : "+name);
+        System.out.println("Age is : " + age);
+        System.out.println("Height is : " + height);
+    }
+
+}
