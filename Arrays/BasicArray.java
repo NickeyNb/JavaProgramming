b94c717c6df0fc6b047fb508ab9b6edf4a109dbc
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-24 11:51:29 +0530

    1d array

--- Arrays/BasicArray.java
+++ Arrays/BasicArray.java
@@ -1,1 +1,30 @@
-// Arrays in java
+// Arrays in java = List of items of the same type(zero-indexed)
+
+import java.util.*;
+
+public class BasicArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size : ");        
+        int size = sc.nextInt();    // arr.length
+
+        // Initializing the array
+        int[] arr = new int[size];
+        // int[] arr = [1, 2, 3, 4];    // declaring and defining
+
+        // input
+        System.out.println("Enter the numbers ");
+        for(int i=0;i<size;i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        // output
+        System.out.print("The numbers in array are : ");
+        for(int i=0;i<size;i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        sc.close();
+    }    
+}
