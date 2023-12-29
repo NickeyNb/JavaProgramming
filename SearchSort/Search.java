8d6b4c08d8204c90213c3e8c8163f9f4b5f1e1ec
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-29 09:55:06 +0530

    minimum number

--- SearchSort/Search.java
+++ SearchSort/Search.java
@@ -4,6 +4,18 @@
 import java.util.*;
 
 public class Search {
+    // find minimum number
+    static int minimumNumber(ArrayList<Integer> arr) {
+        int mn=arr.get(0);
+        for(int val:arr) {
+            if(val < mn) {
+                mn=val;
+            }
+        }
+
+        return mn;
+    }
+
     // search in a string
     static boolean stringSearch(String str, char target) {
         if(str.length() == 0) {
@@ -38,9 +50,9 @@ public class Search {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
         /*
          * 
-         ArrayList<Integer> arr = new ArrayList<>();
          
          System.out.print("Enter size of array : ");
          int size = sc.nextInt();
@@ -57,14 +69,25 @@ public class Search {
         */
         /*
          * 
+         System.out.println("Enter the word : ");
+         String strArr = sc.next();
+         
+         System.out.println("Enter the char : ");
+         char character = sc.next().charAt(0);
+         
+         System.out.println(stringSearch(strArr, character));
          */
-        System.out.println("Enter the word : ");
-        String strArr = sc.next();
-
-        System.out.println("Enter the char : ");
-        char character = sc.next().charAt(0);
-
-        System.out.println(stringSearch(strArr, character));
-        sc.close();
+        /*
+         * 
+         */
+        System.out.println("Enter the size : ");
+        int size=sc.nextInt();
+        
+        System.out.println("Enter array elements : ");
+        for(int i=0;i<size;i++) {
+            arr.add(sc.nextInt());
+        }
+        System.out.println("Minimum number is : " + minimumNumber(arr));
+         sc.close();
     }
 }
