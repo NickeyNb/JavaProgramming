3fe829ccf9acb88d5de46e4ebc31dcf7bb9bb38c
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-29 10:08:40 +0530

    solid rectangle

--- SearchSort/Search.java
+++ SearchSort/Search.java
@@ -48,6 +48,7 @@ public class Search {
 
         return -1;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arr = new ArrayList<>();
@@ -82,7 +83,7 @@ public class Search {
          */
         System.out.println("Enter the size : ");
         int size=sc.nextInt();
-        
+
         System.out.println("Enter array elements : ");
         for(int i=0;i<size;i++) {
             arr.add(sc.nextInt());
