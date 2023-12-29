93d663d25714c70ecb8727097c614ddfcf3a7788
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-29 09:35:33 +0530

    Search a char in string

--- SearchSort/Search.java
+++ SearchSort/Search.java
@@ -4,6 +4,25 @@
 import java.util.*;
 
 public class Search {
+    // search in a string
+    static boolean stringSearch(String str, char target) {
+        if(str.length() == 0) {
+            return false;
+        }
+        // for(int i=0;i<str.length();i++) {
+        //     if(str.charAt(i) == target) {
+        //         return true;
+        //     }
+        // }
+        // for each loop
+        for(char ch : str.toCharArray()) {
+            if(ch == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // search in the array
     static int linearSearch(ArrayList<Integer> nums, int target) {
         if(nums.size() == 0) {
             return -1;
@@ -19,20 +38,33 @@ public class Search {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        /*
+         * 
+         ArrayList<Integer> arr = new ArrayList<>();
+         
+         System.out.print("Enter size of array : ");
+         int size = sc.nextInt();
+         
+         System.out.println("Enter array elements : ");
+         for(int i=0;i<size;i++) {
+             int element = sc.nextInt();
+             arr.add(element);
+            }
+            
+            System.out.print("Enter target : ");
+            int target = sc.nextInt();
+            System.out.println(linearSearch(arr, target));
+        */
+        /*
+         * 
+         */
+        System.out.println("Enter the word : ");
+        String strArr = sc.next();
 
-        System.out.print("Enter size of array : ");
-        int size = sc.nextInt();
+        System.out.println("Enter the char : ");
+        char character = sc.next().charAt(0);
 
-        System.out.println("Enter array elements : ");
-        for(int i=0;i<size;i++) {
-            int element = sc.nextInt();
-            arr.add(element);
-        }
-        
-        System.out.print("Enter target : ");
-        int target = sc.nextInt();
-        System.out.println(linearSearch(arr, target));
+        System.out.println(stringSearch(strArr, character));
         sc.close();
     }
 }
